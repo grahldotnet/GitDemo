02bdf47c652da468ad4c2ead9f50107f3e327b83
gabrielgrahl <gabrielgrahl@gmail.com>
2019-05-04 15:32:06 -0300

    Initial commit

---
+++ DemoTDD/CalculadoraTests.cs
@@ -0,0 +1,15 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoTDD
+{
+    class CalculadoraTests
+    {
+
+    }
+}
